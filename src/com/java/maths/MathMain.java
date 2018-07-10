e3bd8bfe51fc222907f2a08075fa030b6f370f5f
jaishah <532080@LT036366.cts.com>
2018-07-10 18:18:01 +0530

    New math programs

---
+++ src/com/java/maths/MathMain.java
@@ -0,0 +1,12 @@
+package com.java.maths;
+
+public class MathMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+MathPrograms mat = new MathPrograms();
+mat.checkPrime(498);
+mat.printFibonacci(5);
+	}
+
+}
---
+++ src/com/java/maths/MathPrograms.java
@@ -0,0 +1,43 @@
+package com.java.maths;
+
+public class MathPrograms {
+
+	
+	public void checkPrime(int n){
+		
+		for (int i=2;i<n;i++){
+			if(n%i==0){
+				System.out.println("Not Prime: " + n);
+				break;
+			}else{
+				System.out.println("Prime: " + n);
+				break;
+			}
+		}
+		
+	}
+	
+	public void gcd(int a,int b){
+		
+	}
+	
+	public void printFibonacci(int numTerms){
+		int firstTerm = 0;
+		int cnt=0;
+		int[] arr={};
+		for(cnt=0;cnt<numTerms;cnt++){
+		if(cnt==0){
+			arr[cnt]=firstTerm;
+		}else if (cnt==1){
+			arr[cnt]=arr[cnt-1]+1; 
+		}else{
+			arr[cnt]=arr[cnt-1]+arr[cnt-2];
+		}
+			
+		}
+		for (int i : arr) {
+			System.out.print(","+i);
+		}
+	}
+	
+}
